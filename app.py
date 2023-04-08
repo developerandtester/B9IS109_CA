26d75b5ca90e08265b24a82880dfe940ef1ea2c4
Prathamesh Vyas <vyasprathmesh@gmail.com>
2023-04-08 23:16:05 +0100

    signup page and API added

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import mysql.connector
 import hashlib
 from flask_httpauth import HTTPBasicAuth
 import pandas as pd
+import json
+import http.client
+
 auth = HTTPBasicAuth()
 
 
@@ -44,6 +47,90 @@ def home():
 @app.route("/login",methods = ['GET',"POST"])
 def login():
      return render_template('/login.html',form='')
+ 
+@app.route("/logout",methods = ['GET',"POST"])
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+@app.route("/getAddrFromEIRCode",methods = ['GET',"POST"])
+def getAddrFromEIRCode():    
+    freeformaddress=''
+    if request.method == 'POST':
+        input = request.get_json()
+        EIR= input['eircode']         
+        conn = http.client.HTTPSConnection("api.tomtom.com")
+        payload = ''
+        headers = {}        
+        conn.request("GET", "/search/2/search/"+EIR+".json?key=jsDTi8Erj86YCGzh3dLkRcGh20W0d1oM&limit=1&countrySet=IE&idxSet=Str", payload, headers)
+        res = conn.getresponse()
+        print('2')        
+        data = res.read()        
+        res_dict = json.loads(data.decode("utf-8"))
+        freeformaddress = res_dict['results'][0]['address']['freeformAddress']
+        print(freeformaddress)
+    return jsonify({'Result': freeformaddress})
+ 
+def check_sql_injection(text):
+    keywords = ['select', 'insert', 'update', 'delete', 'drop', 'alter', 'create', 'rename', 'truncate']
+    for keyword in keywords:
+        if keyword in text.lower():
+            return True
+    return False
+
+# check for user existence
+def check_user_existence(username):
+    cursor = mydb.cursor()
+    query = "SELECT * FROM users WHERE userName = %s"
+    cursor.execute(query, (username,))
+    user = cursor.fetchone()
+    if user is not None:
+        return True
+    return False
+
+# insert user data into the database
+def insert_user_data(data):
+    cursor = mydb.cursor()
+    query = "INSERT INTO users (userPass, userFirstName, userLastName, userName, userDefaultAddr, userPhone, userAccess) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (data['userPass'], data['userFirstName'], data['userLastName'], data['userName'], data['userDefaultAddr'], data['userPhone'], 2))
+    mydb.commit()
+
+# Flask route to handle the signup form
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # get form data
+        userPass = request.form['userPass']
+        userFirstName = request.form['userFirstName']
+        userLastName = request.form['userLastName']
+        userName = request.form['userName']
+        userDefaultAddr = request.form['userDefaultAddr']
+        userPhone = request.form['userPhone']
+        
+        
+        # check for SQL injection
+        if check_sql_injection(userPass) or check_sql_injection(userFirstName) or check_sql_injection(userLastName) or check_sql_injection(userName) or check_sql_injection(userDefaultAddr) or check_sql_injection(userPhone):
+            return 'Error: SQL injection detected!'
+        
+        # check for user existence
+        if check_user_existence(userName):
+            return 'User already exists!'
+        
+        # insert user data into the database
+        data = {
+            'userPass': userPass,
+            'userFirstName': userFirstName,
+            'userLastName': userLastName,
+            'userName': userName,
+            'userDefaultAddr': userDefaultAddr,
+            'userPhone': userPhone
+        }
+        insert_user_data(data)
+        
+        return 'User created successfully!'
+    else:
+        return render_template('signup.html')
+
  
 @app.route("/verifyUser", methods = ['GET',"POST"])
 def verifyUser():
@@ -95,6 +182,30 @@ def addToCart():
                 print(df)
                 session['cart'] = df.to_json()
         return jsonify({'Result': '1'})
+    
+    
+@app.route('/update_cart', methods=['POST'])
+def update_cart():
+    data = request.get_json()
+    item_id = int(data['id'])
+    new_qty = int(data['new_qty'])
+    
+    dataCart = pd.read_json(session['cart'])
+    
+    # Check if item is in the cart
+    if item_id in dataCart['item'].unique():
+        # Update quantity and amount for the item
+        dataCart.loc[dataCart['item'] == item_id, 'qty'] += new_qty
+        
+        # Check if new quantity is 0 and remove item from cart
+        if dataCart.loc[dataCart['item'] == item_id, 'qty'].values[0] == 0:
+            dataCart = dataCart[dataCart['item'] != item_id]        
+        session['cart'] = dataCart.to_json()
+        return jsonify({'Result': '1'})
+    else:
+        # Item not found in the cart
+        return jsonify({'Result': '0'})
+
 
 @app.route("/cart")
 def cart():
