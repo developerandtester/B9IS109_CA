67d6aa4cfc6dcf87432c8c14245a2a0d8b0d634b
Prathamesh Vyas <vyasprathmesh@gmail.com>
2023-04-08 16:22:53 +0100

    cart functions done

--- app.py
+++ app.py
@@ -26,8 +26,15 @@ mydb = mysql.connector.connect(
 
 @app.route("/", methods = ['GET',"POST"])
 @app.route("/index", methods = ['GET',"POST"])
-def home():  
-    session['cart']=pd.DataFrame(columns=['item','qty']).to_json()
+def home():
+    if('is_logged_in' not in session):
+        session['is_logged_in']=False
+    else:
+        session['is_logged_in']=session['is_logged_in']
+    if('cart' not in session):
+        session['cart']=pd.DataFrame(columns=['item','qty','itemName','price','amount']).to_json()    
+    else:
+        session['cart']=session['cart']
     cursor = mydb.cursor()
     cursor.execute('SELECT * FROM pract_database.tbl_users')
     data = cursor.fetchall()
@@ -61,31 +68,46 @@ def addToCart():
         data = request.get_json()
         print(data)
         item = int(data['id'])
-        qty = int(data['qty'])
-        data2=[[item,qty]]
+        itemName = (data['name'])
+        qty = int(data['qty'])        
         dataCart=pd.read_json(session['cart'])
-        df=pd.DataFrame(dataCart,columns=['item','qty'])
+        itemsJson = pd.read_json('Static\json\menuDetails.json')
+        price=float(itemsJson[itemsJson['id'] == item]['Value'])
+        amount=float(price*qty)
+        data2=[[item,qty,itemName,price,amount]]
+        df=pd.DataFrame(dataCart,columns=['item','qty','itemName','price','amount'])        
         #print(df)
         if(df.empty):
-            df=pd.DataFrame(data2,columns=['item','qty'])
+            df=pd.DataFrame(data2,columns=['item','qty','itemName','price','amount'])
             session['cart'] = df.to_json()                        
         else:
             if((item in df['item'].unique())==True):
                 new_qty=int(df[df['item'] == item]['qty'])+qty
+                new_amount=float(price*new_qty)
                 df.loc[df['item'] == item, 'qty'] = new_qty
+                df.loc[df['item'] == item, 'amount'] = new_amount
                 df_new=df[df['item'] == item]
                 print((int(df_new['qty'])))
                 session['cart'] = df.to_json()        
             else:
-                df_new=pd.DataFrame(data2,columns=['item','qty'])
+                df_new=pd.DataFrame(data2,columns=['item','qty','itemName','price','amount'])
                 df=pd.concat([df,df_new],ignore_index=True)
                 print(df)
                 session['cart'] = df.to_json()
         return jsonify({'Result': '1'})
 
 @app.route("/cart")
-def cart():    
-     return render_template('/cart.html',cart=session['cart'])
+def cart():
+    return render_template('/cart.html',cart=pd.read_json(session['cart']))
+
+@app.route("/checkout", methods = ['GET',"POST"])
+def checkout():
+    if(session['is_logged_in'] == False):
+        return redirect(url_for('login'))
+    elif('is_logged_in' not in session):
+        return redirect(url_for('login'))
+    else:
+        return render_template('/checkout.html',cart=pd.read_json(session['cart']))
    
 @app.route("/successful", methods = ['GET',"POST"])
 def successful():
